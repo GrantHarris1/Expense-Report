f233af6b003deabc57f57893cae8f47fcd712e43
Grant Harris <87549967+GrantHarris1@users.noreply.github.com>
2022-01-09 18:08:57 -0500

    props/components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,11 @@
 import './ExpenseItem.css'
-function ExpenseItem() {
-    const enxpenseDate = new Date(2022, 1, 9);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
+function ExpenseItem(props) {
     return (
         <div className='expense-item'>
-            <div>{enxpenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
         </div>
     );
