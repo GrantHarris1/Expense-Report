f56681a6edf2bec705cef3187b78ccb5c92696f9
Grant Harris <87549967+GrantHarris1@users.noreply.github.com>
2022-02-17 21:24:16 -0500

    small changes

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
-
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  // function clickHandler() {}
+  
   const [title, setTitle] = useState(props.title);
   console.log('ExpenseItem evaluated by React');
   
@@ -21,7 +20,7 @@ const ExpenseItem = (props) => {
         <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+       <button onClick={clickHandler}>Change Title</button> 
     </Card>
   );
 }
